4c742bab3a3ac1e8ef89ddb7af72d936ff70df36
Tasedit <tasedit@gmail.com>
2025-01-29 13:10:35 -0700

    Creating the methods page and working on the display board method

---
+++ Methods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission4
+{
+    internal class Methods
+    {
+        string[] positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        public string Board()
+        {
+            string board = "";
+            for (int i = 0; i < positions.Length; i++)
+            {
+
+            }
+
+            return board;
+        }
+    }
+}
