db6571f2c7325596643b579aa142825f4b7f2750
CarsonTanner <you@example.com>
2025-01-29 13:57:02 -0700

    Draft 2 of main program.cs

--- Program.cs
+++ Program.cs
@@ -1,38 +1,55 @@
-﻿
+﻿namespace Mission4;
+
 using System.ComponentModel.DataAnnotations;
 
-Method m = new Method();
-
-string[] gameBoard = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-Tuple gameResult;
-int position;
-
-Console.WriteLine("Welcome to the Tic-Tac-Toe Game!\n");
-
-do
+internal class Program
 {
-    m.DisplayBoard(gameBoard);
+    internal static void Main(string[] args)
+    {
+        Methods m = new Methods();
 
-    Console.WriteLine("Player 1 Please enter a number below to mark a position on the game board: ");
-    position = int.Parse(Console.ReadLine());
+        string[] gameBoard = new string[9] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        var gameResult = (true , "");
+        string position;
 
-    gameBoard[position - 1] = "X";
+        Console.WriteLine("Welcome to the Tic-Tac-Toe Game!\n");
 
-    m.DisplayBoard(gameBoard);
+        do
+        {
+            m.DisplayBoard(gameBoard);
 
-    Console.WriteLine("\nPlayer 2 Please enter a number below to mark a position on the game board: ");
-    position = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Player 1 Please enter a number below to mark a position on the game board: ");
+                position = Console.ReadLine();
 
-    gameBoard[position - 1] = "O";
+            } while (!m.ValidateGuess(position, gameBoard));
 
-    gameResult = gameOver(gameBoard);
+            gameBoard[int.Parse(position) - 1] = "X";
 
-} while (!gameResult.Item1);
+            m.DisplayBoard(gameBoard);
 
-DisplayWinner();
+            do
+            {
+                Console.WriteLine("\nPlayer 2 Please enter a number below to mark a position on the game board: ");
+                position = Console.ReadLine();
 
-string DisplayWinner()
-{
-    return Console.WriteLine($"Congragulations {gameResult.Item2}. You Won!");
+            } while (!m.ValidateGuess(position, gameBoard));
+
+            gameBoard[int.Parse(position) - 1] = "O";
+
+            gameResult = m.GameOver(gameBoard);
+
+        } while (!gameResult.Item1);
+
+        Console.WriteLine(DisplayWinner());
+
+        string DisplayWinner()
+        {
+            return ($"Congragulations {gameResult.Item2}. You Won!");
+        }
+    }
 }
+
+
 
