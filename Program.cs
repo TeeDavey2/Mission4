d6fd2872a8b4e3dc0a5f9853a38a6bd78038ed49
CarsonTanner <you@example.com>
2025-01-29 13:10:54 -0700

    started coding

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] gameBoard = new string[9] {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+string[] game = new string[9];
+
+Console.WriteLine("Welcome to the Tic-Tac-Toe Game!\n");
+
+Console.Write($" {gameBoard[0]} |");
+Console.Write($" {gameBoard[1]} |");
+Console.Write($" {gameBoard[2]} \n");
+
+Console.WriteLine("-----------");
+
+Console.Write($" {gameBoard[3]} |");
+Console.Write($" {gameBoard[4]} |");
+Console.Write($" {gameBoard[5]} \n");
+
+Console.WriteLine("-----------");
+
+Console.Write($" {gameBoard[6]} |");
+Console.Write($" {gameBoard[7]} |");
+Console.Write($" {gameBoard[8]} ");
+
+for ( int i = 0; i < gameBoard.Length; i++ )
+{
+
+}
+
