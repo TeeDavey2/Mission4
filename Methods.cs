91a682f1f16682bf5b2fdd228d2748370a3ee0b5
Tasedit <tasedit@gmail.com>
2025-01-29 13:34:55 -0700

    Finished methods DisplayBoards() and ValidiateGuess()

--- Methods.cs
+++ Methods.cs
@@ -8,16 +8,40 @@ namespace Mission4
 {
     internal class Methods
     {
-        string[] positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        public string Board()
+        public void DisplayBoard(string[] gameBoard)
         {
-            string board = "";
-            for (int i = 0; i < positions.Length; i++)
-            {
+            Console.Write($" {gameBoard[0]} |");
+            Console.Write($" {gameBoard[1]} |");
+            Console.Write($" {gameBoard[2]} \n");
 
+            Console.WriteLine("-----------");
+
+            Console.Write($" {gameBoard[3]} |");
+            Console.Write($" {gameBoard[4]} |");
+            Console.Write($" {gameBoard[5]} \n");
+
+            Console.WriteLine("-----------");
+
+            Console.Write($" {gameBoard[6]} |");
+            Console.Write($" {gameBoard[7]} |");
+            Console.Write($" {gameBoard[8]} ");
+        }
+
+        public bool ValidateGuess(string guess, string[] gameBoard)
+        {
+            bool result = true;
+            if (guess.Length != 1 | Char.IsLetter(guess[0]) | guess[0] <= 0)
+            {
+                Console.WriteLine("Sorry, the guess must be between a number 1-9. Try again!");
+                result = false;
             }
 
-            return board;
+            else if (!gameBoard.Contains(guess))
+            {
+                Console.WriteLine("This number has already been taken. Try again!");
+                result = false;
+            }
+            return result;
         }
     }
 }
